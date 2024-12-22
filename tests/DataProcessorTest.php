2034c5f9d33ec2acd1125ea5ca48fd4466bead14
Holger Schletz <holger.schletz@web.de>
2024-12-22 16:55:28 +0100

    Add more test cases.

--- tests/DataProcessorTest.php
+++ tests/DataProcessorTest.php
@@ -414,13 +414,26 @@ class DataProcessorTest extends TestCase
         $this->process(['foo' => []], $dataObject);
     }
 
-    public function testClassPropertyValid()
+    public function testClassPropertyValidWithSameClass()
     {
         $dataObject = new class
         {
             public stdClass $foo;
         };
         $value = new stdClass();
+        $result = $this->process(['foo' => $value], $dataObject);
+        $this->assertSame($value, $result->foo);
+    }
+
+    public function testClassPropertyValidWithSubclass()
+    {
+        $dataObject = new class
+        {
+            public stdClass $foo;
+        };
+        $value = new class extends stdClass
+        {
+        };
         $result = $this->process(['foo' => $value], $dataObject);
         $this->assertSame($value, $result->foo);
     }
@@ -477,6 +490,28 @@ class DataProcessorTest extends TestCase
         };
         $result = $this->process([], $dataObject);
         $this->assertEquals('bar', $result->foo);
+    }
+
+    public function testDefaultNullForTypedProperty()
+    {
+        $dataObject = new class
+        {
+            public mixed $foo = null;
+        };
+        $result = $this->process([], $dataObject);
+        $this->assertNull($result->foo);
+    }
+
+    public function testDefaultNullForTypelessProperty()
+    {
+        $dataObject = new class
+        {
+            /** @phpstan-ignore missingType.property */
+            public $foo = null;
+        };
+        $this->expectException(AssertionFailedException::class);
+        $this->expectExceptionMessage('Missing key: foo');
+        $this->process([], $dataObject);
     }
 
     public function testMissingKeyForTypedProperty()
